5d5ca8b95fb389b0d03944642dda90a0447c5926
OlyDimova <49117947+OlyDimova@users.noreply.github.com>
2019-05-01 15:23:47 +0300

    Add files via upload

---
+++ About/About.js
@@ -0,0 +1,11 @@
+window.onload= function() {
+  AboutMe.classList.add('hereAboutMe'),
+    AboutM.classList.add('hereAboutM'),
+    line1.classList.add('move1'),
+    line2.classList.add('move2'),
+    line3.classList.add('move3'),
+    line4.classList.add('move4'),
+    line5.classList.add('move5'),
+    line6.classList.add('move6');
+
+}
